564dfbc72757ed85bd3e06d6223558c7f98884cd
jh4578 <116768893+jh4578@users.noreply.github.com>
2024-03-22 10:31:27 -0400

    Update update_user.py

--- update_user.py
+++ update_user.py
@@ -57,7 +57,7 @@ def app():
     # Handle Search
     if search_user:
         search_query = """
-        SELECT wechat_id, preference, chatbot_wx_id, sche_listing, is_group, no_building, user_id
+        SELECT wechat_id, preference, chatbot_wx_id, sche_listing, is_group, no_building, user_id, conversation
         FROM user
         WHERE 1=1
         """
